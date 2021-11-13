0e0523234b8503fc036730d3991dfeaf9c999a0b
Vojta Bulant <vojtech@bulant.cz>
2021-11-13 21:01:30 +0100

    add src, remove styles and dummy content

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Exchange</title>
+      </Head>
+      <main>
+        foo.
+      </main>
+    </>
+  )
+}
+
+export default Home
